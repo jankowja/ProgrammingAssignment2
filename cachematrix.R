f6aded83fb9f6c3500efd1cb9408374fd51f5e00
jankowja <jankowja@gmail.com>
2016-04-09 15:51:40 -0700

    My first commit for Prog Assingment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## this sets up global objects / variables for the Cachesolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##this function creates a list of 4 functions
+        mtx <- NULL
+        set <- function(y) {
+                x <<- y
+                mtx <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(cachedinv) mtx <<- cachedinv
+        getmatrix <- function() mtx
+        list(set = set, get = get,
+             setmatrix = setmatrix ,
+             getmatrix = getmatrix )
 }
 
 
-## Write a short comment describing this function
+## This function will calculate the inverse of a matrix as an input parameter
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ##this function will calculate an inverse of the matrix
+        ##passed in.  If it's not already stored, it calculates on the fly
+        ##if it already stored in cache, it simply pulls from cache
+        mtx = x$getmatrix()
+        if(!is.null(mtx)) { ##if inverse already stored in cache, pull from cache
+                message("This time inverse pulled from cache!")
+                return(mtx)
+        }
+        mat.data <- x$get()
+        ##calculate the inverse of the matrix:
+        inverse <- solve(mat.data, ...)
+        x$setmatrix(inverse)
+        return(inverse) ## this is calculating the inverse when not already in cache
 }
